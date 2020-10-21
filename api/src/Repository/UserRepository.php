aeb689701c59d923f98e8fb2e8bc21b8b3c811c2
duvg <duviel7@gmail.com>
2020-10-21 00:49:10 -0500

    Add and config api platform, lexik_jwt_authentication, doctrine, make, monolog, base repository, user repository, endpoint for register users, segurity of application, custom exceptions for user register endpoint, services for encoders and request

---
+++ api/src/Repository/UserRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\User;
+use App\Exceptions\User\UserNotFoundException;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+
+class UserRepository extends BaseRepository
+{
+
+    protected static function entityClass(): string
+    {
+        return User::class;
+    }
+
+    // Search user by email
+    public function findOneByEmailOrFail(string $email): User
+    {
+        if (null === $user = $this->objectRepository->findOneBy(['email' => $email])) {
+            throw UserNotFoundException::fromEmail($email);
+        }
+
+        return $user;
+    }
+
+    /**
+     * @param User $user
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function save(User $user): void
+    {
+        $this->saveEntity($user);
+    }
+
+    /**
+     * @param User $user
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(User $user): void
+    {
+        $this->removeEntity($user);
+    }
+}
