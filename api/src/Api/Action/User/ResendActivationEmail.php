6cd02135cde16da4a8a9577c66fed3f6d895463e
duvg <duviel7@gmail.com>
2020-10-22 01:14:23 -0500

    Configure the resend activation email service, handle activated accounts and show throw exception

---
+++ api/src/Api/Action/User/ResendActivationEmail.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Api\Action\User;
+
+
+use App\Service\User\ResendActivationEmailService;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class ResendActivationEmail
+{
+    /**
+     * @var ResendActivationEmailService
+     */
+    private ResendActivationEmailService $resendActivationEmailService;
+
+    public function __construct(ResendActivationEmailService $resendActivationEmailService)
+    {
+        $this->resendActivationEmailService = $resendActivationEmailService;
+    }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $this->resendActivationEmailService->resend($request);
+
+        return new JsonResponse(['message' => 'Activation email sent']);
+    }
+
+}
--- api/src/Service/User/ResendActivationEmailService.php
+++ api/src/Service/User/ResendActivationEmailService.php
@@ -5,60 +5,50 @@ declare(strict_types=1);
 namespace App\Service\User;
 
 
-use App\Entity\User;
-use App\Exceptions\User\UserAlreadyExistException;
+use App\Exceptions\User\UserIsActiveException;
 use App\Messenger\Message\UserRegisteredMessage;
 use App\Messenger\RoutingKey;
 use App\Repository\UserRepository;
-use App\Service\Password\EncoderService;
 use App\Service\Request\RequestService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
 use Symfony\Component\Messenger\MessageBusInterface;
 
-class UserRegisterService
+class ResendActivationEmailService
 {
     /**
      * @var UserRepository
      */
     private UserRepository $userRepository;
     /**
-     * @var EncoderService
-     */
-    private EncoderService $encoderService;
-    /**
      * @var MessageBusInterface
      */
     private MessageBusInterface $messageBus;
 
-    public function __construct(UserRepository $userRepository, EncoderService $encoderService, MessageBusInterface $messageBus)
+    public function __construct(UserRepository $userRepository, MessageBusInterface $messageBus)
     {
 
         $this->userRepository = $userRepository;
-        $this->encoderService = $encoderService;
         $this->messageBus = $messageBus;
     }
 
-    public function create(Request $request): User
+    public function resend(Request $request): void
     {
-        $name = RequestService::getField($request, 'name');
+        // Find user by email
         $email = RequestService::getField($request, 'email');
-        $password = RequestService::getField($request, 'password');
+        $user = $this->userRepository->findOneByEmailOrFail($email);
 
-        $user = new User($name, $email);
-        $user->setPassword($this->encoderService->generateEncodedPassword($user, $password));
-
-        try {
-            $this->userRepository->save($user);
-        } catch ( \Exception $exception) {
-            throw UserAlreadyExistException::fromEmail($email);
+        // throw exception if user already active
+        if ($user->isActive()) {
+            throw UserIsActiveException::fromEmail($email);
         }
+
+        // set a new token
+        $user->setToken(\sha1(\uniqid()));
 
         $this->messageBus->dispatch(
             new UserRegisteredMessage($user->getId(), $user->getName(), $user->getEmail(), $user->getToken()),
             [new AmqpStamp(RoutingKey::USER_QUEUE)]
         );
-
-        return $user;
     }
 }
