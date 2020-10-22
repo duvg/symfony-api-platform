84efecb2361becccf6b1b128f9b2ecb2959c4e64
duvg <duviel7@gmail.com>
2020-10-21 22:14:45 -0500

    Configure service to activate user account

--- api/src/Service/User/UserRegisterService.php
+++ api/src/Service/User/UserRegisterService.php
@@ -7,10 +7,14 @@ namespace App\Service\User;
 
 use App\Entity\User;
 use App\Exceptions\User\UserAlreadyExistException;
+use App\Messenger\Message\UserRegisteredMessage;
+use App\Messenger\RoutingKey;
 use App\Repository\UserRepository;
 use App\Service\Password\EncoderService;
 use App\Service\Request\RequestService;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class UserRegisterService
 {
@@ -22,12 +26,17 @@ class UserRegisterService
      * @var EncoderService
      */
     private EncoderService $encoderService;
+    /**
+     * @var MessageBusInterface
+     */
+    private MessageBusInterface $messageBus;
 
-    public function __construct(UserRepository $userRepository, EncoderService $encoderService)
+    public function __construct(UserRepository $userRepository, EncoderService $encoderService, MessageBusInterface $messageBus)
     {
 
         $this->userRepository = $userRepository;
         $this->encoderService = $encoderService;
+        $this->messageBus = $messageBus;
     }
 
     public function create(Request $request): User
@@ -44,6 +53,11 @@ class UserRegisterService
         } catch ( \Exception $exception) {
             throw UserAlreadyExistException::fromEmail($email);
         }
+        $token = $user->getToken();
+        $this->messageBus->dispatch(
+            new UserRegisteredMessage($user->getId(), $user->getName(), $user->getEmail(), $token),
+            [new AmqpStamp(RoutingKey::USER_QUEUE)]
+        );
 
         return $user;
     }
