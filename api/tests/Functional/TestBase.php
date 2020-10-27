15e278e7894a5fa3a2ea3360e0d0887cf210ea12
duvg <duviel7@gmail.com>
2020-10-27 00:03:44 -0500

    Add packages for fixtures and testing, test for login action

---
+++ api/tests/Functional/TestBase.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DBALException;
+use Hautelook\AliceBundle\PhpUnit\RecreateDatabaseTrait;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class TestBase extends WebTestCase
+{
+    use FixturesTrait;
+    use RecreateDatabaseTrait;
+
+    protected static ?KernelBrowser $client = null;
+    protected static ?KernelBrowser $yamid = null;
+    protected static ?KernelBrowser $carlos = null;
+    protected static ?KernelBrowser $jose = null;
+
+    protected function setUp(): void
+    {
+        if (null === self::$client) {
+            self::$client = static::createClient();
+            self::$client->setServerParameters(
+                [
+                    'CONTENT_TYPE' => 'application/json',
+                    'HTTP_ACCEPT' => 'application/ld+json',
+                ]
+            );
+        }
+
+        if (null === self::$yamid) {
+            self::$yamid = clone self::$client;
+            $this->createAuthenticatedUser(self::$yamid, 'yamid@api.com');
+        }
+
+        if (null === self::$carlos) {
+            self::$carlos = clone self::$client;
+            $this->createAuthenticatedUser(self::$carlos, 'carlos@api.com');
+        }
+
+
+        if (null === self::$jose) {
+            self::$jose = clone self::$client;
+            $this->createAuthenticatedUser(self::$jose, 'jose@api.com');
+        }
+
+
+    }
+
+    private function createAuthenticatedUser(KernelBrowser &$client, string $email): void
+    {
+        $user = $this->getContainer()->get('App\Repository\UserRepository')->findOneByEmailOrFail($email);
+        $token = $this
+            ->getContainer()
+            ->get('Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface')
+            ->create($user);
+
+        $client->setServerParameters(
+            [
+                'HTTP_Authorization' => \sprintf('Bearer %s', $token),
+                'CONTENT_TYPE' => 'application/json',
+                'HTTP_ACCEPT' => 'application/ld+json',
+            ]
+        );
+    }
+
+    protected function getResponseData(Response $response): array
+    {
+        return \json_decode($response->getContent(), true);
+    }
+
+    protected function initDbConnection(): Connection
+    {
+        return $this->getContainer()->get('doctrine')->getConnection();
+    }
+
+    /**
+     * @return false|mixed
+     *
+     * @throws DBALException
+     */
+    protected function getPeterId()
+    {
+        return $this->initDbConnection()->query('SELECT id FROM user WHERE email = "peter@api.com"')->fetchColumn(0);
+    }
+
+    /**
+     * @return false|mixed
+     *
+     * @throws DBALException
+     */
+    protected function getBrianId()
+    {
+        return $this->initDbConnection()->query('SELECT id FROM user WHERE email = "brian@api.com"')->fetchColumn(0);
+    }
+}
---
+++ api/tests/Functional/User/LoginActionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\User;
+
+
+use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationFailureResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class LoginActionTest extends UserTestBase
+{
+    public function testLogin(): void
+    {
+        $payload = [
+            'username' => 'yamid@api.com',
+            'password' => 'password'
+        ];
+
+        self::$yamid->request('POST', \sprintf('%s/login_check', $this->endpoint), [], [], [], \json_encode($payload));
+
+        $response = self::$yamid->getResponse();
+
+        $this->assertEquals(JsonResponse::HTTP_OK, $response->getStatusCode());
+    }
+
+    public function testUserWithInvalidCredentials(): void
+    {
+        $payload = [
+            'username' => 'yamid@api.com',
+            'password' => 'invalid'
+        ];
+
+        self::$yamid->request('POST', \sprintf('%s/login_check', $this->endpoint), [], [], [], \json_encode($payload));
+        
+        $response = self::$yamid->getResponse();
+
+        $this->assertEquals(JsonResponse::HTTP_UNAUTHORIZED, $response->getStatusCode());
+        $this->assertInstanceOf(JWTAuthenticationFailureResponse::class, $response);
+    }
+}
